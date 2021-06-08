3f15fb4e39ca72f0a9c7aab9c393a7ad77ebd589
Sk Shoyeb <52367870+shoyeb001@users.noreply.github.com>
2021-06-08 22:15:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function opennav(){
+    document.getElementById("opennav").style.display = "none";
+    document.getElementById("ul").style.right="0%";
+    
+}
+function closenav() {
+    document.getElementById("ul").style.right="-77%";
+    document.getElementById("opennav").style.display="block";
+    
+}
